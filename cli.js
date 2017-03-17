8daab9a4c3a687a342ef2e36b1cff2951f6d4a17
Dario Vladovic <d.vladimyr@gmail.com>
2017-03-17 15:04:57 +0100

    Initial implementation
    - scraping shoutcast stream info from auto-generated status page

---
+++ cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const chalk = require('chalk');
+const fetchStreamInfo = require('./index.js');
+
+let url = process.argv[2];
+
+const reProto = /^https?:\/\//;
+if (!reProto.test(url)) url = `http://${ url }`;
+
+fetchStreamInfo(url, (err, info) => {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+
+  let title = info['Stream Title'] || info['Stream Name'];
+  if (title) console.log('%s\n', chalk.bold.yellow(title));
+
+  Object.keys(info).forEach(key => {
+    let value = info[key];
+    if (key === 'Content Type') value = chalk.yellow(value);
+    console.log('%s %s', chalk.blue(`${ key }:`), value);
+  });
+});
---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const request = require('request');
+const minidom = require('minidom');
+const noop = Function.prototype;
+
+const ua = 'Mozilla/5.0';
+
+module.exports = fetchStreamInfo;
+
+function fetchStreamInfo(streamUrl, cb=noop) {
+  let headers = { 'User-Agent': ua };
+  request.get(streamUrl, { headers }, (err, { body: html }) => {
+    if (err) {
+      cb(err);
+      return;
+    }
+
+    let $doc = minidom(html);
+    let info = parseStatusPage($doc);
+    cb(null, info);
+  });
+}
+
+function parseStatusPage($doc) {
+  let $tables = $doc.getElementsByTagName('table');
+  let $content = [].filter.call($tables, $it => $it.getAttribute('align') === 'center')[0];
+  let $lines = $content.getElementsByTagName('tr');
+  let data = {};
+  [].forEach.call($lines, $line => {
+    let [prop, ...value] = $line.textContent.trim().split(/:\s+/);
+  	data[prop] = value.join('');
+	});
+  return data;
+}
